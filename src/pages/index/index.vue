dca98f302d0fc38f883cc54b935de57efdf7d9d6
zhilei <hebkia@.com>
2023-04-22 01:14:28 +0800

    提交测试代码

---
+++ src/pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <view class="index">
+    <button @tap="test">
+      Hello World
+    </button>
+    <view>
+      {{index}}
+    </view>
+      
+      
+  </view>
+</template>
+
+<script>
+import { ref } from 'vue'
+import './index.scss'
+
+let i = 0
+
+export default {
+
+  data(){
+    return {
+      index:0
+    }
+  },
+  methods:{
+    test(){
+      setTimeout(()=>{
+        this.index = this.index + 1
+      },100)
+    }
+  },
+  setup () {
+  }
+}
+</script>
